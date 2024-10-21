13febd5925f9d72e043082477a67f999c24af0d1
Ihilesen Egbokhare <75250481+ihilesen@users.noreply.github.com>
2024-10-21 11:29:43 +0100

    Add files via upload

---
+++ Select statement Tutorial.sql
@@ -0,0 +1,12 @@
+SELECT *
+FROM employee_demographics;
+
+SELECT first_name, 
+last_name, 
+birth_date,
+age,
+age + 10
+FROM employee_demographics;
+
+Select Distinct first_name, gender
+From employee_demographics;
