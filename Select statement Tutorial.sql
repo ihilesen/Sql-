ffd2465945bf033c7a03a65782767e90f5fc385e
Ihilesen Egbokhare <75250481+ihilesen@users.noreply.github.com>
2024-10-21 11:33:04 +0100

    Add files via upload

--- Select statement Tutorial.sql
+++ Select statement Tutorial.sql
@@ -1,3 +1,10 @@
+-- Used to work with columns and select what columns you want to bring out
+-- we add a semi colon to let SQL know that's the end of the query (;)
+-- you can also select specific columns  by their names. and separated by a comma. 
+-- you can also have each column in a new row. 
+-- also calculations are possible using (PEMDAS)
+-- When you use DISTINCT, it gives all the unique values in the columns
+
 SELECT *
 FROM employee_demographics;
 
